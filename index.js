228fbbbfc6d8840538615b71904ea8f550156adb
7fishbowls <7fishbowls@gmail.com>
2024-10-29 16:12:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connectDB = require("./config/connectdb");
+const cors = require("cors");
+const fetch_researches = require("./routes/fetch_researches");
+const fetch_photos = require("./routes/fetch_photos");
+const post_research = require("./routes/post_research");
+const post_image = require("./routes/post_image");
+
+const app = express();
+app.use(cors());
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.use("/fetch_researches", fetch_researches);
+app.use("/fetch_photos", fetch_photos);
+app.use("/post_research", post_research);
+app.use("/post_image", post_image);
+
+app.listen(5000);
